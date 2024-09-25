86613bb507057a40151dda7d79a72d5436b5e781
hridayaprajapati <hridaya.prajapati@aadimcollege.edu.np>
2024-09-25 08:24:27 +0545

    [FRONTEND] Finish up Profile page

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,7 +1,46 @@
 const ProfilePage = () => {
   return (
     <>
-      <main></main>
+      <main className="p-6">
+        <div className="px-4 sm:px-0">
+          <h3 className="text-xl font-bold leading-7 text-gray-900">
+            User Information
+          </h3>
+        </div>
+        <div className="user-info flex flex-col items-center justify-around sm:flex-row">
+          <div className="user-image">
+            <img src="#" alt="user-image" className="h-16 w-16 rounded-full" />
+          </div>
+          <div>
+            <dl className="divide-y divide-gray-100">
+              <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                <dt className="text-sm font-medium leading-6 text-gray-900">
+                  Name
+                </dt>
+                <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                  HariKrishna
+                </dd>
+              </div>
+              <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                <dt className="text-sm font-medium leading-6 text-gray-900">
+                  Username
+                </dt>
+                <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                  username123
+                </dd>
+              </div>
+              <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                <dt className="text-sm font-medium leading-6 text-gray-900">
+                  Email
+                </dt>
+                <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                  margotfoster@example.com
+                </dd>
+              </div>
+            </dl>
+          </div>
+        </div>
+      </main>
     </>
   );
 };
