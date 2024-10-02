a23e48ec2904b1cc5fc63aab9e92e256969af280
hridayaprajapati <hridaya.prajapati@aadimcollege.edu.np>
2024-10-02 10:06:37 +0545

    Transition to Inter sans as per TailwindCSS documentation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["InterVariable", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
